096cbbf94381a07833f04389535f8b36709ee798
aquila62 <aquila62@users.noreply.github.com>
2016-08-18 12:53:38 -0500

    Added tower.mak

--- deinst.sh
+++ deinst.sh
@@ -3,3 +3,5 @@ make -f hx2c.mak clean
 make -f eko.mak clean
 make -f ascii.mak clean
 make -f hx.mak clean
+make -f tower.mak clean
+make -f tst.mak clean
--- inst.sh
+++ inst.sh
@@ -3,3 +3,5 @@ make -f hx2c.mak
 make -f eko.mak
 make -f ascii.mak
 make -f hx.mak
+make -f tower.mak
+make -f tst.mak
