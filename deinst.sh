51bfbd1477fb8ca504599068693b0ebd536f31f7
aquila62 <aquila62@users.noreply.github.com>
2016-08-21 12:32:32 -0500

    Added twr.asm program

--- deinst.sh
+++ deinst.sh
@@ -4,4 +4,4 @@ make -f eko.mak clean
 make -f ascii.mak clean
 make -f hx.mak clean
 make -f tower.mak clean
-make -f tst.mak clean
+make -f twr.mak clean
--- inst.sh
+++ inst.sh
@@ -4,4 +4,4 @@ make -f eko.mak
 make -f ascii.mak
 make -f hx.mak
 make -f tower.mak
-make -f tst.mak
+make -f twr.mak
